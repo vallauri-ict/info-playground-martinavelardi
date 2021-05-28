c2e242968d8bf03ea39240c087bf786a40cae2ec
Stud. Martina Velardi <m.velardi.1394@vallauri.net>
2021-05-28 10:21:38 +0200

    Caricato es 38

---
+++ 38_IA01_TaxiFarePrediction/README.md
@@ -0,0 +1,3 @@
+# info-playground-martinavelardi-IA01
+_Velardi Martina 4^B inf_
+### Primo esercizio sull'Intelligenza Artificiale.
---
+++ 38_IA01_TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.ML;
+using System;
+using System.IO;
+
+namespace TaxiFarePrediction
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+        static TaxiTrip taxiTripSample = new TaxiTrip()
+        {
+            VendorId = "VTS",
+            RateCode = "1",
+            PassengerCount = 1,
+            TripTime = 1140,
+            TripDistance = 3.75f,
+            PaymentType = "CRD",
+            FareAmount = 0 // To predict. Actual/Observed = 15.5
+        };
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext(seed: 0);
+
+            // Training del modello
+            var model = Train(mlContext, _trainDataPath);
+
+            // Valutazione del modello
+            Evaluate(mlContext, model);
+
+            // Uso del modello per stimare una corsa
+            TestSinglePrediction(mlContext, model);
+        }
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(dataPath, hasHeader: true, separatorChar: ',');
+            var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripDistance", "PaymentTypeEncoded"))
+                .Append(mlContext.Regression.Trainers.FastTree());
+            var model = pipeline.Fit(dataView);
+            return model;
+        }
+
+        private static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(_testDataPath, hasHeader: true, separatorChar: ',');
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics evaluation         ");
+            Console.WriteLine($"*------------------------------------------------");
+            Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+        }
+
+        private static void TestSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(model);
+            var prediction = predictionFunction.Predict(taxiTripSample);
+            Console.WriteLine($"**********************************************************************");
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+            Console.WriteLine($"**********************************************************************");
+        }
+    }
+}
